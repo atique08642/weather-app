86dcc071d433019e66b0c6d9219d8971ceb51551
Atique Ansari <atique.ansari08642@gmail.com>
2022-09-06 14:27:43 +0530

    Added the getWeather method

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { MainComponent } from './main/main.component';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from "@angular/common/http";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    MainComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -7,14 +8,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MainComponent implements OnInit {
 
-  city_name = "kolkata" ; 
-  constructor() { }
+  city_name = "Mumbai" ; 
+  data:any;
+  constructor(private http:HttpClient) { }
 
   ngOnInit(): void {
+    this.getWeather();
   }
 
-  getCity() {
-      this.city_name = "Mumbai";
+  getWeather() {
+    var baseUrl = "https://api.openweathermap.org/data/2.5/weather?q="+this.city_name+"&appid=3588fa3d74fff694563c18e6f0f8d821";
+    this.http.get(baseUrl)
+        .subscribe(
+          res => {
+            this.data = res;
+          },
+          err => {
+            alert("error");
+          }
+        )
   }
 
 }
